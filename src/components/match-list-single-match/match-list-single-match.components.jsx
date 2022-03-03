956a6a2e3c24f09ad3c5e751404a011e2e62bcdb
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-03 11:09:45 -0600

    adding assets folder and image in match list component

--- src/components/match-list-single-match/match-list-single-match.components.jsx
+++ src/components/match-list-single-match/match-list-single-match.components.jsx
@@ -4,7 +4,11 @@ import "./match-list-single-match.styles.scss";
 
 const SingleMatch = () => (
     <div className="single-match-container">
-        
+        <div className="img-container">
+            <img src="../../assets/user-images/steve-j.jpg" alt="Steve jobs"/>
+        </div>
+        <div></div>
+        <div></div>
     </div>
 )
 
