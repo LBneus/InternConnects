0dcda35af9896328e0973472180610e07e51e390
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-03 11:28:05 -0600

    testing website design with pictures on different desktop sizes

--- src/components/match-list-single-match/match-list-single-match.components.jsx
+++ src/components/match-list-single-match/match-list-single-match.components.jsx
@@ -5,7 +5,7 @@ import "./match-list-single-match.styles.scss";
 const SingleMatch = () => (
     <div className="single-match-container">
         <div className="img-container">
-            <img src="../../assets/user-images/steve-j.jpg" alt="Steve jobs"/>
+            <img src={require('../../assets/user-images/steve-j.jpg')} alt="steve jobs"/>
         </div>
         <div></div>
         <div></div>
