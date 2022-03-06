512cea279938a30b728c1db8e1c7f63bb3e7fc35
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-05 23:06:47 -0600

    adding changes to bio and functionalities components

--- src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/functions-matched-user/functions-matched-user.components.jsx
+++ src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/functions-matched-user/functions-matched-user.components.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./functions-matched-user.styles.scss";
 
 const MatchedUserFunctions = () => (
-    <div className="matched-user-functions-container"></div>
+    <div className="matched-user-functions-container">
+        <div className=""></div>
+        <div className="matched-user-more"></div>
+        <div className=""></div>
+    </div>
 )
 
 export default MatchedUserFunctions;
