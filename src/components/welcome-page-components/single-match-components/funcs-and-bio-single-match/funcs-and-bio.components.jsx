0a4c6adc5c306589b4626589ea29f2cae1b6249f
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-06 20:45:35 -0600

    adding star icon and funcs basic design

--- src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/funcs-and-bio.components.jsx
+++ src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/funcs-and-bio.components.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import MatchedUserBio from "./bio-matched-user/bio-matched-user";
+import MatchedUserFunctions from "./functions-matched-user/functions-matched-user.components";
 
 import "./funcs-and-bio.styles.scss";
 
 const FuncsAndBio = () => (
     <div className="functions-and-bio-container">
+        <MatchedUserFunctions/>
         <MatchedUserBio/>
     </div>
 )
--- src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/functions-matched-user/functions-matched-user.components.jsx
+++ src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/functions-matched-user/functions-matched-user.components.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import {ReactComponent as StarMatchedUser} from "../../../../../icons/star-matched-user.svg";
 import "./functions-matched-user.styles.scss";
 
 const MatchedUserFunctions = () => (
     <div className="matched-user-functions-container">
-        <div className=""></div>
-        <div className="matched-user-more"></div>
-        <div className=""></div>
+        <div className="matched-user-star-icon">
+            <StarMatchedUser className="star-icon-for-matched-user"/>
+        </div>
+        <div className="more-details-matched-user">More Details</div>
+        <div className="contact-matched-user">Contact Steve</div>
     </div>
 )
 
