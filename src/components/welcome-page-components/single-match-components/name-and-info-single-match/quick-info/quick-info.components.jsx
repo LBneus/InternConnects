5a19abdd35c6ff482705d4c01c8b52aeb6f2650f
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-07 14:21:41 -0600

    adding somo quick facts structure

--- src/components/welcome-page-components/single-match-components/name-and-info-single-match/quick-info/quick-info.components.jsx
+++ src/components/welcome-page-components/single-match-components/name-and-info-single-match/quick-info/quick-info.components.jsx
@@ -4,7 +4,11 @@ import "./quick-info.styles.scss";
 
 const QuickInfo = () => (
     <div className="quick-facts-container">
-        <div>Quick Facts</div>
+        <div className="quick-facts-category-name">Quick Facts</div>
+        <div className="quick-facts-dates">Dates: June 21, 2022 to September 2, 2022</div>
+        <div className="quick-facts-university">University: Vanderbilt University Class of 2025</div>
+        <div className="quick-facts-majors">Majors: Computer Science</div>
+        <div className="quick-facts-hometown">Hometown: St Louis, Mo </div>
     </div>
 )
 
