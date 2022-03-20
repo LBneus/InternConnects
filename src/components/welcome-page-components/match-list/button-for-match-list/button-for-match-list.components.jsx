efc3508311b31a97b5319df9b8a6b29710218aec
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-20 18:51:43 -0500

    adding contact and more details custom buttons with little starting design

---
+++ src/components/welcome-page-components/match-list/button-for-match-list/button-for-match-list.components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./button-for-match-list.styles.scss";
+
+const ButtonMatchList = ({ButtonName}) => (
+    <button className="button-match-list">
+        {ButtonName}
+    </button>
+)
+
+export default ButtonMatchList;
--- src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/functions-matched-user/functions-matched-user.components.jsx
+++ src/components/welcome-page-components/single-match-components/funcs-and-bio-single-match/functions-matched-user/functions-matched-user.components.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 import {ReactComponent as StarMatchedUser} from "../../../../../icons/star-matched-user.svg";
+import ButtonMatchList from "../../../match-list/button-for-match-list/button-for-match-list.components";
+
 import "./functions-matched-user.styles.scss";
 
 const MatchedUserFunctions = () => (
     <div className="matched-user-functions-container">
+
         <div className="matched-user-star-icon">
             <StarMatchedUser className="star-icon-for-matched-user"/>
         </div>
-        <div className="more-details-matched-user">More Details</div>
-        <div className="contact-matched-user">Contact Steve</div>
+
+        <div className="more-details-cover">
+            <ButtonMatchList className="more-details-matched-user" ButtonName={"More Details"}/>
+        </div>
+
+        <div className="contact-matched-cover">
+            <ButtonMatchList className="contact-matched-user" ButtonName={"Contact"}/>
+        </div>
+        
     </div>
 )
 
