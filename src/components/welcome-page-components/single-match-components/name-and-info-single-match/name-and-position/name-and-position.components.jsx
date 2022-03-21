b35b5cde3a4564d8cda357cf8f7b550c447821ce
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-21 02:06:05 -0500

    adding some minor design changes

--- src/components/welcome-page-components/single-match-components/name-and-info-single-match/name-and-position/name-and-position.components.jsx
+++ src/components/welcome-page-components/single-match-components/name-and-info-single-match/name-and-position/name-and-position.components.jsx
@@ -4,7 +4,11 @@ import "./name-and-position.styles.scss";
 
 const NameAndPosition = ({userName, position}) => (
     <div className="name-and-position">
-            <div className="matched-user-name-and-position"><span className="matched-user-name">{userName}</span> -&nbsp; <span>{position}</span></div>
+            <div className="matched-user-name-and-position">
+                <span className="matched-user-name">{userName}</span>
+                &nbsp;-&nbsp; 
+                <span>{position}</span>
+            </div>
     </div>
 )
 
