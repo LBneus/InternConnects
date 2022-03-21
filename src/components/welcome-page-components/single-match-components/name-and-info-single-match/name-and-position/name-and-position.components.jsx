1615fecdf54d31adddc004cdd8c22a8e79e18a94
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-21 01:58:43 -0500

    finalizing first design structure for connection page

--- src/components/welcome-page-components/single-match-components/name-and-info-single-match/name-and-position/name-and-position.components.jsx
+++ src/components/welcome-page-components/single-match-components/name-and-info-single-match/name-and-position/name-and-position.components.jsx
@@ -4,8 +4,7 @@ import "./name-and-position.styles.scss";
 
 const NameAndPosition = ({userName, position}) => (
     <div className="name-and-position">
-            <div className="match-user-name">{userName} -</div>
-            <div className="match-user-position">- {position}</div>
+            <div className="matched-user-name-and-position"><span className="matched-user-name">{userName}</span> -&nbsp; <span>{position}</span></div>
     </div>
 )
 
--- src/components/welcome-page-components/single-match-components/name-and-info-single-match/quick-info/quick-info.components.jsx
+++ src/components/welcome-page-components/single-match-components/name-and-info-single-match/quick-info/quick-info.components.jsx
@@ -5,10 +5,31 @@ import "./quick-info.styles.scss";
 const QuickInfo = () => (
     <div className="quick-facts-container">
         <div className="quick-facts-category-name">Quick Facts</div>
-        <div className="quick-facts-dates">Dates: June 21, 2022 to September 2, 2022</div>
-        <div className="quick-facts-university">University: Vanderbilt University Class of 2025</div>
-        <div className="quick-facts-majors">Majors: Computer Science</div>
-        <div className="quick-facts-hometown">Hometown: St Louis, Mo </div>
+
+        <div className="quick-facts-dates"> Dates: 
+            <span className="matched-user-date">
+                June 21, 2022 to September 2, 2022
+            </span>
+        </div>
+
+        <div className="quick-facts-university"> University: 
+            <span className="matched-user-university">
+                Vanderbilt University Class of 2025
+            </span>
+        </div>
+
+        <div className="quick-facts-majors"> Major(s): 
+            <span className="matched-user-major">
+                Computer Science
+            </span>
+        </div>
+
+        <div className="quick-facts-hometown"> Hometown: 
+            <span className="matched-user-hometown">
+                St Louis, Mo 
+            </span>
+        </div>
+        
     </div>
 )
 
