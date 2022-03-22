f3ac88f35e6f97cd3c18b2bd2237205cde2f032e
lbneus <luka.buskivadze@vanderbilt.edu>
2022-03-21 20:46:25 -0500

    changing sign out button design in accounts page

--- src/pages/accounts-page/accounts-page.pages.jsx
+++ src/pages/accounts-page/accounts-page.pages.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ProfileForAccountsPage from "../../components/accounts-page-components/profiles-for-accounts-page/profiles-for-accounts.components";
 import CreateProfileAccounts from "../../components/accounts-page-components/create-profile-accounts-page/create-profile-accounts-page.components";
 
+import { Link } from "react-router-dom";
+
 
 import "./accounts-page.styles.scss"
 
@@ -15,12 +17,16 @@ const AccountsPage = () => (
         <h5 className="account-information-email">
             alexander.j.erwin@vanderbilt.edu
         </h5> 
-        <button className="account-page-signout-button">Sign Out</button>
 
+        <Link to="/">
+            <button className="account-page-signout-button">Sign Out</button>
+        </Link>
+        
         <div className="accounts-profiles-container">
             <ProfileForAccountsPage className="already-created-profile"/>
             <CreateProfileAccounts className="create-new-profile"/>
         </div>
+        
 
         <h6 className="listing-message">(select profile to view listings)</h6>
     </div>
