28684d9eee093b2820ea296d435eb1a0ce7ecc88
Alexander Erwin <alexanderwin37@gmail.com>
2022-03-29 11:13:54 -0500

    adding backend code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Route, BrowserRouter} from 'react-router-dom';
 
 import WelcomePage from './pages/welcome-page/welcome-page.pages';
 import ConnectionPage from './pages/connection-page/connection-page.pages';
 import AccountsPage from './pages/accounts-page/accounts-page.pages';
+import {UserContext} from "./UserContext";
 
-function App() {
-  return (
-    <div>
-      <Switch>
-          <Route exact path='/' component={WelcomePage}/>
-          <Route exact path='/connectionPage' component={ConnectionPage}/>
-          <Route exact path='/accountsPage' component={AccountsPage}/>
-      </Switch>
-    </div>
-  );
+export default function App() {
+    return (
+        <UserContext.Provider value={""}>
+            <BrowserRouter>
+                <Route exact path='/' component={WelcomePage}/>
+                <Route exact path='/connectionPage' component={ConnectionPage}/>
+                <Route exact path='/accountsPage' component={AccountsPage}/>
+            </BrowserRouter>
+        </UserContext.Provider>
+    )
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div>
+//       <Switch>
+//           <Route exact path='/' component={WelcomePage}/>
+//           <Route exact path='/connectionPage' component={ConnectionPage}/>
+//           <Route exact path='/accountsPage' component={AccountsPage}/>
+//       </Switch>
+//     </div>
+//   );
+// }
+//
+// export default App;
--- src/pages/welcome-page/welcome-page.pages.jsx
+++ src/pages/welcome-page/welcome-page.pages.jsx
@@ -1,22 +1,157 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useContext} from "react";
 
 import "./welcome-page.styles.scss"
 
+import {UserContext, useUserContext} from "../../UserContext";
+import {Link} from "react-router-dom";
 
-const WelcomePage = () => (
+class WelcomePage extends React.Component {
 
-    <div className="welcome-page">
-        <h1 className="welcome-message">Welcome to InternConnects</h1>
-        <h3 className="access-information-message">
-            InternConnects is only available to those with current .edu email address
-        </h3>
-        <input type="email" className="user-email" placeholder={"Enter your email"}/>
-        <input type="password" className="user-password" placeholder={"Enter your password"}/>
-        <Link to="/accountsPage">
-            <button className="log-in-button">Log In</button>
-        </Link>
-    </div>
-)
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+
+            email:'',
+            password:'',
+            firstName:'',
+            lastName:'',
+            newAccount: false,
+
+            userUUID:'',
+            upidCount: 0,
+            userProfiles:[],
+            response:''
+        }
+        this.toggleAccountCreate = this.toggleAccountCreate.bind(this);
+        this.sendLoginRequest = this.sendLoginRequest.bind(this);
+        this.sendCreateAccountRequest = this.sendCreateAccountRequest.bind(this)
+    }
+
+    setEmailVal = e => {
+        this.setState({ email: e.currentTarget.value });
+    }
+
+    setPasswordVal = e => {
+        this.setState({ password: e.currentTarget.value });
+    }
+
+    setFirstName = e => {
+        this.setState({ firstName: e.currentTarget.value });
+    }
+
+    setLastName = e => {
+        this.setState({ lastName: e.currentTarget.value });
+    }
+
+    toggleAccountCreate() {
+        this.setState({ newAccount: !this.state.newAccount });
+    }
+
+    sendLoginRequest() {
+        // const acctEmail = this.state.email;
+        // const acctPassword = this.state.password;
+        // let jsonRequest = JSON.stringify({ "userEmail": acctEmail, "hashedPassword": acctPassword })
+        //
+        // let request = new XMLHttpRequest();
+        // request.open('POST', ' https://016oltoux6.execute-api.us-east-1.amazonaws.com/beta/accounts/login');
+        // request.send(jsonRequest);
+        // request.onload = () => {
+        //     response = JSON.parse(request.response);
+        // }
+
+        fetch("https://016oltoux6.execute-api.us-east-1.amazonaws.com/beta/accounts/login", {
+            "method": "POST",
+            "body": JSON.stringify({
+                userEmail: this.state.email,
+                hashedPassword: this.state.password
+            })
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response.success) {
+
+                    const {globalUUID, setGlobalUUID} = useUserContext();
+
+                    this.setState({
+                        userUUID: response.IC_UUID,
+                        upidCount: response.numUPIDs,
+                        userProfiles: response.associatedProfileList
+                    })
+                    setGlobalUUID(response.IC_UUID);
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    sendCreateAccountRequest() {
+        fetch("https://016oltoux6.execute-api.us-east-1.amazonaws.com/beta/accounts", {
+            "method": "POST",
+            "body": JSON.stringify({
+                userEmail: this.state.email,
+                hashedPassword: this.state.password,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName
+            })
+        })
+            .then(response => response.json())
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    render() {
+
+        let changeModeMsg = this.state.newAccount ? "Switch To Log In" : "Switch to Create Account";
+        let submitRequestMsg = this.state.newAccount ? "Create Account" : "Log In";
+
+        return (
+            <div className="welcome-page">
+                <h1 className="welcome-message">Welcome to InternConnects</h1>
+                <h3 className="access-information-message">
+                    InternConnects is only available to those with current .edu email address
+                </h3>
+                <input type="email" className="user-email" placeholder={"Enter your email"}
+                       onChange={(e) => this.setEmailVal(e)}/>
+                <input type="password" className="user-password" placeholder={"Enter your password"}
+                       onChange={(e) => this.setPasswordVal(e)}/>
+                { this.state.newAccount &&
+                    <input type="text" className="user-email" placeholder={"Enter your first name"}
+                           onChange={(e) => this.setFirstName(e)}/> }
+                { this.state.newAccount &&
+                    <input type="text" className="user-email" placeholder={"Enter your last name"}
+                           onChange={(e) => this.setLastName(e)}/> }
+                <button type="submit" className="log-in-button"
+                        onClick={this.state.newAccount ? this.sendCreateAccountRequest : this.sendLoginRequest}>{submitRequestMsg}</button>
+                <button className="switch-mode-button" onClick={this.toggleAccountCreate}>{changeModeMsg}</button>
+                <Link to={{
+                    pathname: '/accountsPage',
+                    state: { userEmail: this.state.userEmail, ic_uuid: this.state.ic_uuid, profileCount: this.state.profileCount }
+                }}>
+                {/*<Link to="/accountsPage">*/}
+                    <button className="log-in-button">Passthrough</button>
+                </Link>
+                {/*<h1>UPIDCNT: {this.state.userUUID}</h1>*/}
+            </div>
+        );
+    }
+}
+
+// const WelcomePage = () => (
+//
+//     <div className="welcome-page">
+//         <h1 className="welcome-message">Welcome to InternConnects</h1>
+//         <h3 className="access-information-message">
+//             InternConnects is only available to those with current .edu email address
+//         </h3>
+//         <input type="email" className="user-email" placeholder={"Enter your email"}/>
+//         <input type="password" className="user-password" placeholder={"Enter your password"}/>
+//         <Link to="/accountsPage">
+//             <button className="log-in-button">Log In</button>
+//         </Link>
+//     </div>
+// )
 
 export default WelcomePage;
