c48ac9ae5b9d43cee0bdf2e02cc417af0418b6d9
lbneus <luka.buskivadze@vanderbilt.edu>
2022-04-05 20:18:10 -0500

    commiting small changes

--- src/components/welcome-page-components/header-for-match-list/date-input-info/date-input-info.components.jsx
+++ src/components/welcome-page-components/header-for-match-list/date-input-info/date-input-info.components.jsx
@@ -7,9 +7,10 @@ const DateInput = ({userDateInfo, userDirection}) => (
     
         { 
             userDirection === "From" ? 
-            <div class="date-input-container"><span>From</span><span className="date-input">  {userDateInfo.arrivalMonth} {userDateInfo.arrivalDay}, {userDateInfo.arrivalYear} </span></div>
+            <div class="date-input-container">From <div className="date-input">  {userDateInfo.arrivalMonth} {userDateInfo.arrivalDay}, {userDateInfo.arrivalYear} </div></div>
             :
-            <div class="date-input-container"><span>To</span><span className="date-input">  {userDateInfo.departureMonth} {userDateInfo.departureDay}, {userDateInfo.departureYear} </span></div>
+            <div className="date-input"> {userDateInfo.departureDay} </div>
+                
         }
 
     </div>
